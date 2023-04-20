7969e4f171cf8e5c08f8791c0bf6ab1da7ce7ff0
GovtGeek <26489473+GovtGeek@users.noreply.github.com>
2023-04-19 23:54:34 -0700

    Add files via upload

---
+++ Libraries/VanillaConversion.lua
@@ -0,0 +1,33 @@
+if C_Container == nil then
+	C_Container = {}
+	----[[
+	-- Create an autoload function that will try to call the global namespace
+	setmetatable(C_Container, {__index = function (t, funcname, args) return _G[funcname] end } )
+	--]]
+	function C_Container.GetContainerItemInfo(bagIndex, slotIndex)
+		icon, itemCount, locked, quality, readable, lootable, itemLink, isFiltered, noValue, itemID, isBound = GetContainerItemInfo(bagIndex, slotIndex)
+		containerItemInfo = {
+			["iconFileId"] = icon,
+			["stackCount"] = itemCount,
+			["isLocked"] = locked,
+			["quality"] = quality,
+			["isReadable"] = readable,
+			["hasLoot"] = lootable,
+			["hyperlink"] = itemLink,
+			["isFiltered"] = isFiltered,
+			["hasNoValue"] = noValue,
+			["itemID"] = itemID,
+			["isBound"] = isBound
+		};
+		return containerItemInfo
+	end
+end
+
+if C_Minimap == nil then
+	local C_Minimap = {}
+	setmetatable(C_Minimap, {__index = function (t, funcname, args) return _G[funcname] end } )
+	function C_Minimap.SetTracking(vIndex, pEnabled)
+		-- pEnabled was sometimes set to 1 instead of true. This forces true/false
+		return SetTracking(vIndex, pEnabled == true or pEnabled == 1)
+	end
+end
